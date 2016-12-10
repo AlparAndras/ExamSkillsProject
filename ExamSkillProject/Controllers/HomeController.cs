587ac6936d852a1d3292feffee392d1d0aaf233c
roni0101 <roni94radev@gmail.com>
2016-12-10 17:24:17 +0100

    Company

--- ExamSkillProject/Controllers/HomeController.cs
+++ ExamSkillProject/Controllers/HomeController.cs
@@ -9,20 +9,7 @@ namespace ExamSkillProject.Controllers
 {
     public class HomeController : Controller
     {
-        //Hello from Alpar
-        //hello from roni
-        //hello from Alpar 
 
-
-
-
-
-
-        
-        
-        
-        private SkillsContext db = new SkillsContext();
-        private List<Skill> Skills = new List<Skill>();
         public ActionResult Index()
         {
             return View();
@@ -42,36 +29,6 @@ namespace ExamSkillProject.Controllers
             return View();
         }
 
-        public ActionResult ShowAllSkills()
-        {
-            Skills = this.db.Skills.ToList();
-            return View(Skills);
-        }
 
-        public ActionResult SkillDetails(int id)
-        {
-            Skill skill = this.db.Skills.Find(id);
-            return View(skill);
-        }
-
-        [HttpGet]
-        public ActionResult CreateSkill()
-        {
-            return View("CreateSkill");
-
-        }
-        [HttpPost]
-        public ActionResult Create(Skill skill)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View("CreateSkill", skill);
-            }
-           
-            this.db.Skills.Add(skill);
-            db.SaveChanges();
-            Skills = this.db.Skills.ToList();
-            return View("CreateSkill");
-        }
     }
 }
